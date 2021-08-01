eb7093e1fd1ece64045db83539f515436ab3120e
iVuDang <vdang159@mtroyal.ca>
2021-07-31 18:29:15 -0600

    Uploading completed project - version July 31, 2021

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
-# Project: Rock Paper Scissors (Part I)
-Reference: https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/rock-paper-scissors
+# Project: Rock Paper Scissors (Part II)
+Part I: https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/rock-paper-scissors
+Part II: https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/dom-manipulation#dom-methods
 
 ## Purpose: 
-* make a simple implementation of grade-school classic “rock paper scissors”. 
-* we will revisit this project in a later lesson and add a Graphical User Interface with buttons and text
-* You might notice some ‘Live Preview’ links in the student solutions that have a GUI - this is coming in a later lesson. When you get there don’t forget to come back and add your link!
+* Made a simple implementation of grade-school classic “rock paper scissors”. 
 
 
 ### Technologies: 
@@ -22,20 +21,48 @@ Reference: https://www.theodinproject.com/paths/foundations/courses/foundations/
 
 ### Links 
 Codepen - solution link:
-https://codepen.io/iVuDang/pen/BapgEqB
+https://codepen.io/iVuDang/pen/zYwavJY
+
+GitHub - deployment link: 
 
 <br />
 
 ## Summary
 
 ### What I learned
+####Part I
 * Function names are a verb. Keep concise and descriptive e.g. getS, calcE, createX, checkY. 
 * One function - one action. Write a specific function for each specific purpose (segregrate). 
 * Test each coding line before going onto the next e.g. (console.log)
 
+####Part II
+* Hardest problem overcame in this exercise was to figure out how to register the HTML buttons to our JavaScript function. 
+```
+// B1. Registers the click input to execute the function 'playRound'. References our const 'buttons'. 
+buttons.forEach(button =>{                          // acts on forEach <button> in html
+    button.addEventListener('click', function(){    
+        playRound(button.value)                     // calls on our function playerRound, where the input is contained in our <button> in html as the attribute value=""
+    })
+})
+```
+
+* Can import and use icons from script to kit.fontawesome, and using <i class=""> to reference to the icon ref.  
+``` 
+  <script src="https://kit.fontawesome.com/4d18f3cb78.js" crossorigin="anonymous"></script>
+    
+  <i class="fas fa-fire-alt"> </br> Fire </i>
+```
+
+* Principle of must create the element in JavaScript, and then reference to HTML by document.getElementById, and then append the JavaScript element to the HTML element
+```
+let para = document.createElement('p');
+para.textContent = 'Hey, I\'m red.';
+para.style.color = 'red';
+document.getElementById('container').appendChild(para);
+```
 
 ### What could be improved
-* To add in graphical user interface (Live Preview on GitHub pages)
-* Need to figure out how to console.log the prompt input so that it performs 5 rounds in sequential order. 
-
+* How to change 'leaf' to 'earth' on output. 
+* How to add a time delay effect for the element image and the score results. 
+* Need better understanding and more practice with classList.add("active") and classList.
 
--- script.js
+++ script.js
@@ -1,83 +1,107 @@
-// 3. begin with a function called computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. Tip: use the console to make sure this is returning the expected output before moving to the next step!
+/* ---------- A.SET UP OUR VARIABLES ---------- */
+let playerScore = 0
+let computerScore = 0
+const buttons = document.querySelectorAll('.element-button')
 
+
+/* ---------- B. DOM - BUTTON FUNCTIONALITIES ---------- */
+// B1. Registers the click input to execute the function 'playRound'. References our const 'buttons'. 
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
+
+
+// B2. Disables the buttons if function is called upon (in our playRound function)
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
+
+
+
+/* ---------- C. SEGREGATED JAVASCRIPT FUNCTIONS - ONE FUNCTION ONE ACTION---------- */
+
+// C1. Generate random CPU input 
 function computerPlay(){
-    const compOutputs = ['rock', 'paper', 'scissors'];
-    let compRandom = Math.floor(Math.random() * compOutputs.length);
+  const choices = ['fire', 'leaf', 'water'];
+  let choicesIndex = Math.floor(Math.random() * choices.length); 
   
-    return compOutputs[compRandom];
-  }
+  return choices[choicesIndex];
+};
+
+console.log(computerPlay()); 
+// fire || earth || water 
+
+
+
+// C2. Executes game logic, conditions for user input to win vs. cpu random input
+/* 
+  1. playRound function pulls in (references) two other functions: computerPlay, disableButtons
+  2. playRound function is then pulled into the DOM: button.addEventListener('click', function(){
+          playRound(button.value)
+*/
+
+function playRound(playerSelection) {
+    let computerSelection = computerPlay()
+    let result = ""
+
+    if (playerSelection == computerSelection) {
+        result = '<br>You both harnessed ' + playerSelection
+    } // ENDS if (playerSelection == computerSelection)
   
-  console.log(computerPlay());
+    else if ((playerSelection == 'water' && computerSelection == 'fire') ||
+        (playerSelection == 'leaf' && computerSelection == 'water') ||
+        (playerSelection == 'fire' && computerSelection == 'leaf')) {
+            playerScore += 1
+            result = '<br><b>You win: </b><br>' + playerSelection + ' beats ' + computerSelection
+                
+                // if statement within the execution of an outer if statement 
+                if (playerScore == 5) {
+                    result += '<br><br>Reload the page to train again.'
+                    disableButtons()  // calls on function disableButtons()
+                } // ENDS if (playerScore == 5)
+    } // ENDS if ((playerSelection == 'fire' && computerSelection == 'water')
   
-  
-  // Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-  // Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-  
-  function checkWinner(playerSelection, computerSelection){  
-    computerSelection = computerPlay();                // pulls function computerPlay
-    playerSelection = playerSelection.toLowerCase();  // Link this to alert 
-  
-    let result = '';
+    else {
+        computerScore += 1
+        result = '<br><b>Defeat:</b> <br>' + computerSelection + ' beats ' + playerSelection
+
+            if (computerScore == 5) {
+                result += '<br><br>Reload the page to regain your honor.'
+                disableButtons()  // function disableButtons()
+            } // ENDS if (computerScore == 5)
+    } // ENDS else 
     
-    switch(true){
-      case computerSelection === playerSelection:
-        result = 'tie'
-        break;
+    // calls our function 'function updateChoices(playerSelection, computerSelection)' to update the images within the execution of this function
+    updateChoices(playerSelection, computerSelection); 
   
-      case (playerSelection === 'rock' && computerSelection === 'scissors'):
-      case (playerSelection === 'paper' && computerSelection === 'rock'):
-      case (playerSelection === 'scissors' && computerSelection === 'paper'):
-        result = 'win'
-        break;
+    // references our HTML <div id="result"> and updates the HTML content of the <div> element with what we assigned: result variable
+    document.getElementById('result').innerHTML = result
   
-      default:
-        result = 'lose'
-        break; 
-    } 
-    return [result, playerSelection, computerSelection];
-  }
+    document.getElementById('result-playerscore').innerHTML = `<b>Player:</b><br> ${playerScore}`;
+    document.getElementById('result-computerscore').innerHTML = `<b>Computer:</b><br> ${computerScore}`;
+
+} // ENDS function playRound(playerSelection) 
+
+
+// C3. Update images based on playerSelection and computerSelection - called into playRound function
+function updateChoices(playerSelection, computerSelection) {
+  // create variables to link to our HTML elements
+  const playerElement = document.getElementById("player-element"); 
+  const computerElement = document.getElementById("computer-element"); 
+  // assigns classList property to our variables, than adds 'active' class to our classList 
+  playerElement.classList.add("active");
+  computerElement.classList.add("active"); 
   
-  console.log(checkWinner('ROCK',));
+  // creates a second set of variables to pull our button.value
+  const playerElementClassName = `fa-${playerSelection}`
+  const computerElementClassName = `fa-${computerSelection}`
   
+  playerElement.classList = `fas ${playerElementClassName} active`;
+  computerElement.classList = `fas ${computerElementClassName} active`;
   
-  // 6.Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
-  
-  // GLOBAL VARIABLES 
-  let maxRounds = 5;
-  let playerCount = 0;
-  let compCount = 0; 
-  
-  
-  function playRound(playerMove) {                              // pulls our playerInput prompt
-    let winnerResult = '';
-    winnerResult = checkWinner(playerMove, computerPlay());    // pulls function checkWinner and our prompt 
-    
-    if (winnerResult[0] === 'win' && maxRounds > 0) {   // the output of our function [result, playerSelection, computerSelection], hence, 0 index returns our result
-      playerCount += 1;
-      maxRounds -= 1; 
-      console.log('You Win');
-      
-        } else if (winnerResult[0] === 'lose' && maxRounds > 0) {
-          compCount += 1;
-          maxRounds -= 1; 
-          console.log('You Lose')
-          
-        } else if (winnerResult[0] === 'tie' && maxRounds > 0){
-          maxRounds -= 1; 
-          console.log('Tie')
-    
-        } else {
-          console.log('F5 to Start a New Game')
-        }
-    
-    return [playerCount, compCount, maxRounds, winnerResult];
-    }
-  
-  
-  
-  // 6.3 Use prompt() to get input from the user. 
-  let playerInput = prompt("Input either rock, paper or scissors");
-  
-  console.log(playRound(playerInput));
-  console.log(playRound(playerInput));
-  
+}
+
